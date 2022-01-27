1301826ee30f4fc0aa659ebf7ea1499512834d29
simonkell <SimonKell1505@gmail.com>
2022-01-27 16:35:58 +0100

    Upload config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_NAME', 'Name der Datenbank');
+    define('DB_USER', 'Username für Datenbank');
+    define('DB_PASSWORD', 'Passwort für User');
+    define('DB_HOST', 'Datenbank-Host');
+    define('DSN', 'mysql:host='.DB_HOST.'; dbname='.DB_NAME.'; charset=utf8')
+?>
